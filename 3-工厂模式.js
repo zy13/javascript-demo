5b84dc16540a1fc41ee5c1d2cb1adff9b2e7116b
zy <zengyuan@keywa.com>
2021-03-09 09:49:04 +0800

    工厂模式-同一类对象的封装：类/构造函数

---
+++ 3-工厂模式.js
@@ -0,0 +1,32 @@
+// 字面量定义对象
+let zhangsan = {
+  name: '张三',
+  age: 20,
+  hobby() {
+    console.log('喜欢打篮球')
+  }
+}
+
+let lisi = {
+  name: '李四',
+  age: 21,
+  hobby() {
+    console.log('喜欢打足球')
+  }
+}
+
+// 类（构造函数）：工厂模式(能生成同一类对象)
+function Person(name, age, hobby) {
+  let obj = {} // 添加原料
+  obj.name = name 
+  obj.age = age
+  obj.hobby = function() {
+    console.log(hobby)
+  }
+  // 加工原料
+  return obj // 出厂
+}
+
+// 构造函数创建对象
+let zs = Person('张三', 20, '喜欢篮球')
+let ls = Person('李四', 21, '喜欢足球')
